36a9ea21289773bd700f8e31a409658c73e90763
Yaeesh Babar <yaeeshbabar@gmail.com>
2026-04-23 20:42:54 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.use("/about", (req, res)=>{
+    res.send("About Us")
+})
+
+app.use("/", (req, res)=>{
+    res.send("Hello from Dashbboard...")
+})
+
+// app.use("/aboutt", (req, res) => {
+//     res.send("About Us T");
+// });
+
+// app.use("/", (req, res) => {
+//     res.send("Hello from Dashboard");
+// });
+
+app.listen(3000, ()=>{
+    console.log("Server is successfully listening on port 3000...");
+});
